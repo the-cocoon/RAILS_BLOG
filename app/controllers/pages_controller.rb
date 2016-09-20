053f30522ca8b9215d8bc9e5bd8b1ccb83feb263
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-09-20 22:31:59 +0300

    ~~~

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,15 @@ class PagesController < RailsBlogController
   # For all responses in this controller, return the CORS access control headers.
 
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin']  =  'http://www.getrix.ru'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET'
+    headers['Access-Control-Allow-Origin']  = '*'
+    headers['Access-Control-Allow-Methods'] = '*'
     headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
 
     headers.delete("X-Frame-Options")
     headers.delete("X-Content-Type-Options")
     headers.delete("X-XSS-Protection")
+
+    response.headers.except! 'X-Frame-Options'
   end
 
   def show
