41548e5440e3467e402a9f1ea95b99920060ed67
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-09-22 10:54:23 +0300

    ~~~

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,5 @@
 class PagesController < RailsBlogController
   include ::RailsBlog::PubController
-  after_filter :cors_set_access_control_headers
-
-  # For all responses in this controller, return the CORS access control headers.
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin']  = '*'
-    headers['Access-Control-Allow-Methods'] = '*'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Allow-Credentials'] = true
-
-    headers["X-Frame-Options"] = "ALLOW-FROM http://www.getrix.ru"
-    # headers.delete("X-Content-Type-Options")
-    # headers.delete("X-XSS-Protection")
-
-    # response.headers.except! 'X-Frame-Options'
-  end
 
   def show
     super
